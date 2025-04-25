98e867a3ed6b851be4be2e3e8498f1f8ab9e2445
Marco Ferragina <marco.ferragina@gmail.com>
2025-04-25 20:23:35 +0200

    better released file name

--- extra/log.ps1
+++ extra/log.ps1
@@ -1,5 +1,7 @@
 # Infinite loop to log current date and time to a file
-$logFile = "C:\log.txt"
+
+$dir = (Get-Location).Path
+$logFile = "$dir\log.txt"
 
 while ($true) {
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
